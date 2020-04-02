beddf7ea8b9814a66722b818e2ae646f9481ea1b
quangson91 <quangson91@gmail.com>
2020-04-02 10:05:11 +0700

    Load content of an url.

--- presentation/src/main/java/io/github/jenthone/hackernews/helper/glide/ThumbnailOfUrlDataFetcher.kt
+++ presentation/src/main/java/io/github/jenthone/hackernews/helper/glide/ThumbnailOfUrlDataFetcher.kt
@@ -3,6 +3,10 @@ package io.github.jenthone.hackernews.helper.glide
 import com.bumptech.glide.Priority
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.data.DataFetcher
+import io.github.jenthone.hackernews.domain.helper.exception.DataNotFoundException
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.koin.java.KoinJavaComponent.inject
 import java.io.InputStream
 
 /**
@@ -11,6 +15,9 @@ import java.io.InputStream
 class ThumbnailOfUrlDataFetcher(
     private val model: String
 ) : DataFetcher<InputStream> {
+
+    private val okHttpClient: OkHttpClient by inject(OkHttpClient::class.java)
+
     override fun getDataClass(): Class<InputStream> = InputStream::class.java
 
     override fun cleanup() {
@@ -24,7 +31,16 @@ class ThumbnailOfUrlDataFetcher(
     }
 
     override fun loadData(priority: Priority, callback: DataFetcher.DataCallback<in InputStream>) {
-        // TODO: load content of an url.
+        val request = Request.Builder()
+            .url(model)
+            .build()
+        val content = try {
+            okHttpClient.newCall(request).execute().body?.string()
+                ?: throw DataNotFoundException()
+        } catch (e: Exception) {
+            callback.onLoadFailed(e)
+            return
+        }
 
         // TODO: Parse url thumbnail by using jsoup.
 
