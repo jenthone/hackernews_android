4f209ffc00a148c605d5a9227ff9fe838abbc9aa
Duy Mac <jenthone@gmail.com>
2020-08-31 14:04:46 +0700

    Remove debug logic in release flavor

--- data/src/release/java/io/github/jenthone/hackernews/data/helper/Debugger.kt
+++ data/src/release/java/io/github/jenthone/hackernews/data/helper/Debugger.kt
@@ -1,29 +1,10 @@
 package io.github.jenthone.hackernews.data.helper
 
 import android.app.Application
-import com.facebook.stetho.Stetho
-import com.facebook.stetho.okhttp3.StethoInterceptor
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import tech.linjiang.pandora.Pandora
-import timber.log.Timber
 
-fun Application.attachToDebugger() {
-    Stetho.initializeWithDefaults(this)
-    Timber.plant(Timber.DebugTree())
-}
+fun Application.attachToDebugger() = Unit
 
-fun Application.openDebugger() {
-    Pandora.get().open()
-}
+fun Application.openDebugger()  = Unit
 
-fun OkHttpClient.Builder.attachToDebugger() {
-    val httpLoggingInterceptor = HttpLoggingInterceptor()
-    addInterceptor(
-        httpLoggingInterceptor.apply {
-            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-        }
-    )
-    addNetworkInterceptor(StethoInterceptor())
-    addInterceptor(Pandora.get().interceptor)
-}
+fun OkHttpClient.Builder.attachToDebugger() = Unit
