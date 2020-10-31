e81eb5add85e5791434f1856c65206f855b72938
jenthone <jenthone@gmail.com>
2020-10-31 22:42:15 +0700

    Refactor code style

--- data/src/main/java/io/github/jenthone/hackernews/data/helper/ResultHelper.kt
+++ data/src/main/java/io/github/jenthone/hackernews/data/helper/ResultHelper.kt
@@ -2,16 +2,14 @@ package io.github.jenthone.hackernews.data.helper
 
 import timber.log.Timber
 
-inline fun <T> safeResult(block: () -> T): Result<T> {
-    return safeResult(block) { it }
-}
+inline fun <T> safeResult(block: () -> T): Result<T> =
+    safeResult(block) { it }
 
-inline fun <T, E> safeResult(block: () -> T, map: (T) -> E): Result<E> {
-    return try {
+inline fun <T, E> safeResult(block: () -> T, map: (T) -> E): Result<E> =
+    try {
         val data = block()
         Result.success(map(data))
     } catch (e: Throwable) {
         Timber.e(e)
         Result.failure(e)
     }
-}
